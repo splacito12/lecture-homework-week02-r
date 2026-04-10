9468b3eef879dca081c298ff232764984419fda9
Samantha Placito Melendrez <95974510+splacito12@users.noreply.github.com>
2026-04-10 16:52:26 +0000

    finished

--- endianness_converter.sv
+++ endianness_converter.sv
@@ -1,1 +1,26 @@
 // Put your code below
+module endianness_converter(
+    input clk,
+    input [31:0] word,
+    input endianness_in,
+    input endianness_out,
+
+    output logic [31:0] converted_word
+);
+
+always_ff @(posedge clk) begin
+    //code goes here
+    if(endianness_in == endianness_out) begin
+        converted_word <= word;
+    end
+    else begin
+        converted_word <= {
+            word[7:0],
+            word[15:8],
+            word[23:16],
+            word[31:24]
+        };
+    end
+end
+
+endmodule
